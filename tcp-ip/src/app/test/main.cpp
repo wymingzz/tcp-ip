d38ab36ae4dba28c99826cec49ea2b4f9a4017b8
wymingzz <114494905+wymingzz@users.noreply.github.com>
2023-07-21 18:19:56 +0800

    在debug中实现了assert断言

--- tcp-ip/src/app/test/main.cpp
+++ tcp-ip/src/app/test/main.cpp
@@ -51,6 +51,9 @@ int main(void) {
 	dbg_info(DBG_TEST, "info");
 	dbg_warning(DBG_TEST, "warning");
 	dbg_error(DBG_TEST, "error");
+	dbg_assert(1 == 1, "failed");
+	dbg_assert(1 == 0, "failed");
+
 
 	net_init();
 
